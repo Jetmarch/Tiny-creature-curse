e2fc154b95fe33c935f932ca3fc2d912bff78c5e
Andrew Shibin <j3tmarch@gmail.com>
2024-10-05 19:04:39 +0300

    feat: project files

---
+++ Assets/Code/Game/LilFrogCurses.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ClearCursesProto
+{
+    [Flags]
+    public enum LilFrogCurses : byte
+    {
+        None,
+        Fangs,
+        Wings,
+        Tail,
+        BigNose,
+        Horns
+    }
+}
